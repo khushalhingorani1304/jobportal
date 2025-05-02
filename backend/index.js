af2c45a7b553e6b39d1f3b879402bd196ff91737
Khushal Hingorani <khushalhingorani1304@gmail.com>
2025-05-02 20:55:15 +0530

    add dist

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    process.env.FRONTEND,
+    'https://jobportal-smoky-nu.vercel.app'
 ];
 
 const corsOptions = {
