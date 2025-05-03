61a707891971226a77b19ef23fd4da9397f8251b
Khushal Hingorani <khushalhingorani1304@gmail.com>
2025-05-03 15:36:04 +0530

    Production Build

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 
@@ -16,38 +17,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'https://jobportal-smoky-nu.vercel.app',
-    credentials:true
-}
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  }));
 
+app.options('*', cors());
 
-// const allowedOrigins = [
-//     'http://localhost:5173',
-//     'http://127.0.0.1:5173',
-//     'https://jobportal-smoky-nu.vercel.app'
-// ];
-
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         // Allow requests with no origin (like mobile apps or curl)
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     credentials: true
-// };
-
-app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
-
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
+const _dirname = path.resolve();
 
 
 // api's
@@ -56,6 +36,11 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_,res) =>{
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
+});
 
 
 app.listen(PORT,()=>{
