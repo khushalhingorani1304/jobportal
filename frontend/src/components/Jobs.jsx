1c6baf922f42344158d1c3b45927f39f2ba0dd9d
Khushal Hingorani <khushalhingorani1304@gmail.com>
2025-05-04 12:52:29 +0530

    done some changes

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -29,13 +29,13 @@ const Jobs = () => {
             <Navbar />
             <div className='max-w-7xl mx-auto mt-5'>
                 <div className='flex gap-5'>
-                    <div className='w-20%'>
+                    <div className='w-[20%] hidden md:block'>
                         <FilterCard />
                     </div>
                     {
                         filterJobs.length <= 0 ? <span>Job not found</span> : (
                             <div className='flex-1 h-[88vh] overflow-y-auto pb-5'>
-                                <div className='grid grid-cols-3 gap-4'>
+                                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
                                     {
                                         filterJobs.map((job) => (
                                             <motion.div
